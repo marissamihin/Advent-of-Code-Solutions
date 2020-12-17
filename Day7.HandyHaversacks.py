b9ba471768f9f798b2dfc84fcb0630f625987a5d
marissamihin <marissamihin@gmail.com>
2020-12-17 11:47:56 -0800

    Fixed key: value pairs

--- Day7.HandyHaversacks.py
+++ Day7.HandyHaversacks.py
@@ -19,14 +19,27 @@ input_list = Path("Day7Input.txt").resolve().read_text()
 # under these rules, shiny gold bags must contain 126 bags total.
 
 def data_parser(raw_list):
-	#takes in all of the data, splits each line into lists
-	return [line.split() for line in raw_list.splitlines()]
+	split_lines = [line.split() for line in raw_list.splitlines()]
+	for line in split_lines:
+		line.remove('contain')
+		line.remove('bags')
+		for i in range(len(line)):
+			if 'bag,' in line:
+				line.remove('bag,')
+			if 'bags,' in line:
+				line.remove('bags,')
+		line.pop(-1)
+	return split_lines
 
 
 def Convert_string_to_dictionary(lst):
 	main_dict = {}
 	for line in lst:
-		sub_bags = tuple(line[4:])
+		sub_bag_list = line[2:]
+		sub_bag_types = int(len(sub_bag_list) / 3)
+		sub_bags = []
+		for i in range(sub_bag_types):
+			sub_bags += [sub_bag_list[i*3:(i+1)*3]]
 		res_dct = {(line[0], line[1]): sub_bags}
 		main_dict.update(res_dct)
 	return main_dict
@@ -35,29 +48,22 @@ def Convert_string_to_dictionary(lst):
 def bag_count(bag, main_dict):
 	count = 0
 	# print(bag, 'this is the bag')
-	sub_bag_types = int(len(bag) / 4)
-	# print(sub_bag_types)
-	for i in range(0, sub_bag_types):
+	sub_bags = main_dict[bag]
+	for i in range(0, len(sub_bags)):
 		sub_bag = main_dict[bag[((i*4) + 1):((i*4)+3)]]
 		# print(sub_bag, 'this is the sub_bag')
-		if bag[0] == 'no':
-			return 1
-		if sub_bag[0] == 'no':
-			return 1
-		if sub_bag[3] == 'bags.':
+		if 'no' in sub_bag:
 			return 1
 		else:
-			count += int(sub_bag[(i)*4])*bag_count(sub_bag, main_dict) 
+			count += int(sub_bag[(i)*4])*bag_count(sub_bag[i], main_dict) 
 	return count
 
 
-# I keep getting an "IndexError: tuple index out of range" message
-# and I think I understand WHY, but I'm not sure how to fix it...
-# I may have to scrap the indexing and try some other way
-# there's almost certainly a way to do this nicely....
-
+# Error: unhashable type: list
 
 parsed_data = data_parser(input_list)
+print(parsed_data[0])
 dictionary_data = Convert_string_to_dictionary(parsed_data)
 shiny_gold = dictionary_data[('shiny', 'gold')]
+print(shiny_gold)
 print(bag_count(shiny_gold, dictionary_data))
